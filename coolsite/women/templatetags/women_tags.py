62be07b1ea96df2a0cc96a65925be9f43650ba79
espacio8 <xilene1998@mail.ru>
2025-11-18 19:52:12 +0300

    Created tags

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -1,7 +1,7 @@
 from django import template
 
 from women import views
-from women.models import Category
+from women.models import Category, TagPost
 
 register = template.Library()
 
@@ -10,3 +10,7 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    return {'tags': TagPost.objects.all()}
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 
-from women.models import Women, Category
+from women.models import Women, Category, TagPost
 
 menu = [
     {'title': "О сайте", 'url_name': 'about'},
@@ -68,10 +68,23 @@ def login(request):
 
 def show_category(request, cat_slug):
     category = get_object_or_404(Category, slug=cat_slug)
-    posts = Women.objects.filter(cat_id=category.pk)
+    posts = Women.published.filter(cat_id=category.pk)
     data = {'title': f'Рубрика: {category.name}',
             'menu': menu,
             'posts': posts,
             'cat_selected': category.pk,
             }
     return render(request, 'women/index.html', data)
+
+def show_tag_postlist(request, tag_slug):
+    tag = get_object_or_404(TagPost, slug=tag_slug)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+
+    data = {
+        'title': f'Тег: {tag.tag}',
+        'menu': menu,
+        'posts': posts,
+        'cat_selected': None,  # Синим не отображается выбранная категория
+    }
+
+    return render(request, 'women/index.html', context=data)
